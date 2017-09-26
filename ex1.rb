d979cce45daf3d83f3ed31b1d0416251a5d05d28
2hill <jamil.rahou@laposte.net>
2017-09-26 19:28:32 +0200

    conditions

--- ex1.rb
+++ ex1.rb
@@ -2,5 +2,13 @@ puts "Entrez le chiffre"
 
 
 chiffre = gets.chomp.to_i
+adeviner =12
 
-puts chiffre.even?
+if chiffre > adeviner
+  puts "#{chiffre} est trop grand"
+elsif chiffre < adeviner
+    puts "#{chiffre} est trop petit"
+else chiffre == adeviner
+  puts "Bravo!"
+
+  end
