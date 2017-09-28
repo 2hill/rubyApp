a175a4d4e748f057fa3729b0e8123c8c474afa6f
2hill <jamil.rahou@laposte.net>
2017-09-28 19:52:24 +0200

    sorting table

--- method.rb
+++ method.rb
@@ -1,12 +1,35 @@
-def convert_to_min(seconds)
+=begin def convert_to_min(seconds)
    return seconds / 60
 end
 
-puts convert_to_min(500)
 
 
 def say_hi(name)
    return "hello #{name}"
 end
 
+
+def greetings(name, type)
+   puts "#{type} #{name}"
+end
+
+
+puts convert_to_min(500)
 puts say_hi("Fred")
+puts greetings("Sam", "hello")
+
+=end
+
+
+
+def reorganize_list(ascending, *names)
+   new_name =  names.collect { |name| name.to_s }
+   new_name.sort!
+   new_name.reverse! if !ascending
+   return new_name
+end
+
+#ascending
+puts reorganize_list(true, 'sam', 'fred','john', :eve, 'sandra').inspect
+#descending
+puts reorganize_list(false, 'sam', 'fred','john', :eve, 'sandra').inspect
