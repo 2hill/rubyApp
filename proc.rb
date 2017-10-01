5368f68053c9adfec45ce2baaaa3c687355816ba
2hill <jamil.rahou@laposte.net>
2017-10-01 16:56:25 +0200

    proc exercise followup

--- proc.rb
+++ proc.rb
@@ -1,7 +1,28 @@
-def demo
+=begin def demo
   puts "Hello"
   yield
   puts "Bye Bye"
 end
 
 demo { puts "How's it going?"}
+
+=end
+
+students = [
+  {mark:15, name: 'Fred'},
+  {mark: 2, name: 'Sam'},
+  {mark: 18, name: 'Bob'}
+]
+
+def aboveAvg(students)
+
+students.each do |student|
+   if student[:mark] >= 10
+      yield(student)
+    end
+   end
+  end
+
+aboveAvg (students)do |student|
+  puts "#{student[:name]} has an above average mark"
+end
