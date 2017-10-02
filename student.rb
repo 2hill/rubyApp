9a4d4f9d366f9178e15c16d4efdaeeb6c495aa9a
2hill <jamil.rahou@laposte.net>
2017-10-02 12:36:05 +0200

    updated class 2

--- student.rb
+++ student.rb
@@ -8,10 +8,17 @@ end
 def greeting
   puts "Hi my name is... #{@name} and I'm #{@age}"
 end
+
+def isMajor
+  puts @age > 21 ? " #{@name} is major" : " #{@name} is not major"
+end
+
 end
 
 student1 = Student.new("Slim shady!", 32)
 student1.greeting
+student1.isMajor
 
-student2 = Student.new("Francis", 28)
+student2 = Student.new("Francis", 20)
 student2.greeting
+student2.isMajor
