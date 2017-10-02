0578c948ad75e1b802121665f0a9c149262bc1ba
2hill <jamil.rahou@laposte.net>
2017-10-02 12:29:33 +0200

    updated class

--- student.rb
+++ student.rb
@@ -1,16 +1,17 @@
 class Student
 
-def initialize(name)
+def initialize(name, age)
 @name = name
+@age = age
 end
 
 def greeting
-  puts "Hi my name is... #{@name}"
+  puts "Hi my name is... #{@name} and I'm #{@age}"
 end
 end
 
-student1 = Student.new("Slim shady!")
+student1 = Student.new("Slim shady!", 32)
 student1.greeting
 
-student2 = Student.new("Francis")
+student2 = Student.new("Francis", 28)
 student2.greeting
