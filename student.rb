cca6650079b08ce35d276e3610de4556640c7000
2hill <jamil.rahou@laposte.net>
2017-10-02 12:55:44 +0200

    updated class 3

--- student.rb
+++ student.rb
@@ -1,24 +1,21 @@
 class Student
+   def initialize(name, age)
+      @name = name
+      @age = age
+   end
 
-def initialize(name, age)
-@name = name
-@age = age
-end
+   def greeting
+      puts "Hi my name is... #{@name} and I'm #{@age}"
+      puts isMajor ? " #{@name} is major" : " #{@name} is not major"
+   end
 
-def greeting
-  puts "Hi my name is... #{@name} and I'm #{@age}"
-end
-
-def isMajor
-  puts @age > 21 ? " #{@name} is major" : " #{@name} is not major"
-end
-
+    def isMajor
+       @age >= 21
+    end
 end
 
 student1 = Student.new("Slim shady!", 32)
 student1.greeting
-student1.isMajor
 
 student2 = Student.new("Francis", 20)
 student2.greeting
-student2.isMajor
